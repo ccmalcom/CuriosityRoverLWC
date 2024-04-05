5a58e5aec70e68af39c4c0cbcbc3eb5f175303c5
ccmalcom <chasecmalcom@gmail.com>
2024-04-05 18:04:12 -0400

    more

--- force-app/main/default/lwc/missionControl/missionControl.js
+++ force-app/main/default/lwc/missionControl/missionControl.js
@@ -8,6 +8,9 @@ export default class MissionControl extends LightningElement {
         const { camera, earthDate } = event.detail;
         getRoverPhotos({ camera: camera, earthDate: earthDate })
             .then(result => {
+                console.log('Mars Rover photos:', result);
+                console.log('first photo:', result[0]);
+                console.log('first photo img:', result[0].imgUrl);
                 this.roverPhotos = result;
             })
             .catch(error => {
